7e8b4ae647a5bdcd4d2d829d8717dc10d1ae48f8
Mohamed Gomaa <mido.15897@gmail.com>
2025-06-25 20:40:39 +0300

    End Of Section 9

--- client/src/app/home/home.ts
+++ client/src/app/home/home.ts
@@ -1,6 +1,5 @@
-import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import {  Component } from '@angular/core';
 import { Register } from "../register/register";
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +7,8 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home implements OnInit {
-  http = inject(HttpClient);
-  private cdr = inject(ChangeDetectorRef);
+export class Home {
   registerMode = false;
-  users: any;
-  
-  ngOnInit(): void {
-    this.getUsers();
-  }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
@@ -26,15 +18,4 @@ export class Home implements OnInit {
     this.registerMode = event;
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => {
-        this.users = response
-
-        this.cdr.detectChanges(); // Force Angular to update the view
-      },
-      error: error => console.log(error),
-      complete: () => console.log("Request has Completed")
-    })
-  }
 }
